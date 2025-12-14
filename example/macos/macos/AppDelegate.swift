52ca2d0236aca28d3c67a040d017fa8c95087d8e
Patryk Mleczek <patrykmleczek.pm@gmail.com>
2025-12-14 12:32:21 +0100

    build: initialize macos app at example/macos

---
+++ example/macos/macos/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  macos
+//
+//  Created by Patryk Mleczek on 12/14/25.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ example/macos/macos/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  macos
+//
+//  Created by Patryk Mleczek on 12/14/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
